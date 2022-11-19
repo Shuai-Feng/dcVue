c69e32e087a97032e54dc1bd19c788a65bb17f5c
shuaifeng <shuaifeng121314@126.com>
2022-11-19 09:17:21 +0800

    feat:lesson4end

--- src/layout/DashLayout.vue
+++ src/layout/DashLayout.vue
@@ -9,9 +9,9 @@
                 <sub-menu :menuCfg="menuCfg" />
             </a-menu>
         </a-layout-sider>
-        <a-layout class="layoutContent">
-            <a-header class="layoutHeader scrolled">
-                HEADER
+        <a-layout id="lay_content"  class="layoutContent" :onScroll="onHeaderScroll">
+            <a-header :class="{layoutHeader:true,scrolled:menuTop>30}">
+                Header
             </a-header>
             <a-layout-content style="margin:12px;">
                 <router-view />
@@ -23,8 +23,23 @@
 </template>
 
 <script lang="ts" setup>
+import {ref} from 'vue'
 import menuCfg from './menuCfg'
 import SubMenu from './SubMenu.vue'
+
+const menuTop = ref(0)
+
+
+/**
+ *  方法：滚动条Header显示
+*/
+const onHeaderScroll = (e) => {
+    const scrolTop = document.getElementById('lay_content')?.scrollTop
+    if(scrolTop){
+        menuTop.value=scrolTop
+    }
+}
+
 
 
 </script>
@@ -57,7 +72,7 @@ import SubMenu from './SubMenu.vue'
         position: sticky;
         z-index: 999;
         top: 0px;
-        padding: 12px;
+        padding: 16px;
         background-color: #fff;
         transition: all 0.4s ease-out;
 
@@ -66,7 +81,8 @@ import SubMenu from './SubMenu.vue'
 
         &.scrolled {
             width: calc(100% - 24px);
-            left: 12px
+            left: 12px;
+            box-shadow: 0px 4px 4px  rgba(0,0,0,0.15);
         }
     }
 }
--- src/pages/UI/Button.vue
+++ src/pages/UI/Button.vue
@@ -1,16 +1,104 @@
 <template>
-    <a-card style="height:2000px">
-        <a-button>
-            123
+    <a-card title="基础的按钮" style="margin-top:10px;">
+        <a-button style="margin-right:12px" type="primary">
+            Vue 单车
         </a-button>
-        <a-button>
-            123
+        <a-button style="margin-right:12px" type="danger">
+            Vue 单车
         </a-button>
-        <a-button>
-            123
+        <a-button style="margin-right:12px" type="dashed">
+            Vue 单车
         </a-button>
-        <a-button>
-            123
+        <a-button style="margin-right:12px">
+            Vue 单车
+        </a-button>
+    </a-card>
+    <a-card title="带icon的按钮" style="margin-top:20px;">
+        <a-button style="margin-right:12px">
+            <template #icon>
+                <FileAddOutlined />
+            </template>
+            Vue 单车
+        </a-button>
+        <a-button style="margin-right:12px">
+            <template #icon>
+                <DeleteOutlined />
+            </template>
+            Vue 单车
+        </a-button>
+        <a-button style="margin-right:12px">
+            <template #icon>
+                <SearchOutlined />
+            </template>
+            Vue 单车
+        </a-button>
+        <a-button style="margin-right:12px">
+            <template #icon>
+                <EditOutlined />
+            </template>
+            Vue 单车
+        </a-button>
+    </a-card>
+    <a-card title="loding中的按钮" style="margin-top:20px;">
+        <a-button loading style="margin-right:12px">
+            <template #icon>
+                <FileAddOutlined />
+            </template>
+            Vue 单车
+        </a-button>
+        <a-button loading style="margin-right:12px">
+            <template #icon>
+                <DeleteOutlined />
+            </template>
+            Vue 单车
+        </a-button>
+        <a-button loading style="margin-right:12px">
+            <template #icon>
+                <SearchOutlined />
+            </template>
+            Vue 单车
+        </a-button>
+        <a-button loading style="margin-right:12px">
+            <template #icon>
+                <EditOutlined />
+            </template>
+            Vue 单车
+        </a-button>
+        <a-button shape="round" loading style="margin-right:12px">
+            <template #icon>
+                <EditOutlined />
+            </template>
+
+        </a-button>
+    </a-card>
+    <a-card title="按钮组" style="margin-top:20px;">
+        <a-radio-group>
+            <a-radio-button value="large">
+                <LeftOutlined />
+                左边
+            </a-radio-button>
+            <a-radio-button value="default">
+                <RightOutlined />
+                右边
+            </a-radio-button>
+        </a-radio-group>
+    </a-card>
+    <a-card title="大中小按钮" style="margin-top:20px;">
+        <a-radio-group v-model:value="size">
+            <a-radio value="large">大</a-radio>
+            <a-radio value="middle">中</a-radio>
+            <a-radio value="small">A</a-radio>
+        </a-radio-group>
+        <a-button :size="size">
+            {{size}}
         </a-button>
     </a-card>
 </template>
+<script lang="ts" setup>
+import {ref,onMounted,onUnmounted} from 'vue'
+import { SearchOutlined, EditOutlined, DeleteOutlined, FileAddOutlined, LeftOutlined, RightOutlined } from '@ant-design/icons-vue';
+const size = ref<'large'|'middle'|'small'>('small')
+
+
+
+</script>
